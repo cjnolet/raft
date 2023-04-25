83ad4141ad14c4695577fac7715cd51033595d8b
Simon Adorf <sadorf@nvidia.com>
2023-04-25 06:28:23 -0700

    Use more defensive imports and better names in NB tests module.

--- naive_bayes/tests.py
+++ naive_bayes/tests.py
@@ -1,8 +1,14 @@
-# import numpy as np
-import cunumeric as np
+try:
+    import cunumeric as cn
+except ImportError:
+    raise ImportError("The Naive Bayes tests require cunumeric.")
 from numpy.testing import assert_allclose
-from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import MultinomialNB as skNB
+
+try:
+    from sklearn.metrics import accuracy_score
+    from sklearn.naive_bayes import MultinomialNB as skNB
+except ImportError:
+    raise ImportError("The Naive Bayes tests require scikit-learn.")
 
 from naive_bayes import MultinomialNB
 from naive_bayes.cn.multinomial import MultinomialNB as CNMultinomialNB
@@ -22,8 +28,8 @@ def test_multinomial(nlp_20news):
 
     sk_model.fit(X, y)
     # Cunumeric does not natively support sparse arrays.
-    X_dense = np.ascontiguousarray(X.todense())
-    cn_legate_model.fit(X_dense, y)
+    X_cn_dense = cn.ascontiguousarray(X.todense())
+    cn_legate_model.fit(X_cn_dense, y)
     legate_model.fit(X, y)
     assert_allclose(sk_model.classes_, cn_legate_model.classes_)
     assert_allclose(sk_model.classes_, legate_model.classes_)
@@ -32,16 +38,16 @@ def test_multinomial(nlp_20news):
 
     sk_log_proba = sk_model.predict_log_proba(X)
     legate_log_proba = legate_model.predict_log_proba(X)
-    cn_legate_log_proba = cn_legate_model.predict_log_proba(X_dense)
+    cn_legate_log_proba = cn_legate_model.predict_log_proba(X_cn_dense)
     sk_proba = sk_model.predict_proba(X)
     legate_proba = legate_model.predict_proba(X)
-    cn_legate_proba = cn_legate_model.predict_proba(X_dense)
+    cn_legate_proba = cn_legate_model.predict_proba(X_cn_dense)
     # sk_score = sk_model.score(X, y)
     # legate_score = legate_model.score(X, y)
 
     y_sk = sk_model.predict(X)
     y_legate = legate_model.predict(X)
-    y_cn_legate = cn_legate_model.predict(X_dense)
+    y_cn_legate = cn_legate_model.predict(X_cn_dense)
 
     assert_allclose(cn_legate_log_proba, sk_log_proba, atol=5e-1, rtol=5e-1)
     assert_allclose(cn_legate_proba, sk_proba, atol=2e-1, rtol=2.5)
